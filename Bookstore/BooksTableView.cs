921c0d9af14aa49c203ce90171cf86bda259f29e
Ivant2 <32110248+Ivant2@users.noreply.github.com>
2017-10-18 13:59:58 -0600

    sa

---
+++ Bookstore/BooksTableView.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Bookstore
+{
+    public partial class BooksTableView : UITableView
+    {
+        public BooksTableView (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
---
+++ Bookstore/BooksTableViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Bookstore
+{
+    public partial class BooksTableViewController : UITableView
+    {
+        public BooksTableViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
