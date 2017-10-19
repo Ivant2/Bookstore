ef2993530eaae675925de99c6f8455405569799e
Ivant2 <32110248+Ivant2@users.noreply.github.com>
2017-10-19 14:02:06 -0600

    erg

--- Bookstore/BooksTableViewController.cs
+++ Bookstore/BooksTableViewController.cs
@@ -1,13 +1,50 @@
 using Foundation;
 using System;
+using System.Collections.Generic;
+using System.CodeDom.Compiler;
 using UIKit;
 
 namespace Bookstore
 {
-    public partial class BooksTableViewController : UITableView
+    public partial class BooksTableViewController : UITableViewController
     {
+        List<Book> bookList;
         public BooksTableViewController (IntPtr handle) : base (handle)
         {
+            bookList = new List<Book>();
+
+            bookList.Add(new Book()
+            {
+                Author = "J.K. Rowling",
+                Name = "Harry Potter and the Sorcerer's Stone",
+                Publisher = "Bloomsbury",
+                Year = 1997
+            });
+
+			bookList.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Chamber of Secrets",
+				Publisher = "Bloomsbury",
+				Year = 1998
+			});
+
+			bookList.Add(new Book()
+			{
+				Author = "J.K. Rowling",
+				Name = "Harry Potter and the Sorcerer's Stone",
+				Publisher = "Bloomsbury",
+				Year = 1999
+			});
         }
+
+    }
+
+    public class Book 
+    {
+        public string Name;
+        public string Author;
+        public string Publisher;
+        public int Year;
     }
 }
---
+++ Bookstore/UI.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace Bookstore
+{
+    public partial class UI : UITableViewCell
+    {
+        public UI (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
